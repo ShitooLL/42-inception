e94935209c2b09d9f8fda592981633e18f1d92cb
ShitooLL <Shitoo.ll@gmail.com>
2025-02-08 22:24:04 +0100

    updated from debian:buster to debian:bullseye

--- srcs/requirements/mariadb/tools/start-mysql.sh
+++ srcs/requirements/mariadb/tools/start-mysql.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-service mysql start;
+service mariadb start;
+
+echo "Creating database if does not exist..."
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+echo "Creating user if does not exist..."
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD`}';"
+
+echo "Granting privileges..."
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+echo "Modifying root password..."
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+echo "Applying modifications..."
 mysql -e "FLUSH PRIVILEGES;"
+
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown;
+
+echo "Executing MariaDB in safe mode..."
 exec mysql_safe
